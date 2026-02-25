fb511496ca839a4d6db3c2e5db27241415452e71
bradley <55547314+BradM99@users.noreply.github.com>
2026-02-25 20:48:11 +0000

    rough endpoint setup in main for testing, to be moved

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+import services, db_models, api_schemas
+from db import get_db, engine
+from sqlalchemy.orm import Session
+from api_schemas import *
 
 app = FastAPI()
 
+@app.get("/")
+def root():
+    return {"message": "API is running"}
+
+@app.get("/spots", response_model=list[Spot])
+def get_all_spots(db: Session = Depends(get_db)):
+    return services.get_all_spots(db)
+
+@app.post("spots", response_model=Spot)
+def create_spot(spot: SpotCreate, db: Session = Depends(get_db)):
+    return create_spot(spot, db)
