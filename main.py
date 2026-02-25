0fceea8b592bba618f9a34390a9bae9085a5b3a9
bradley <55547314+BradM99@users.noreply.github.com>
2026-02-25 21:05:45 +0000

    fixed recursive issue

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI, Depends, HTTPException
-import services, db_models, api_schemas
-from db import get_db, engine
+from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
+
+import services
 from api_schemas import *
+from db import get_db
 
 app = FastAPI()
 
@@ -14,6 +15,6 @@ def root():
 def get_all_spots(db: Session = Depends(get_db)):
     return services.get_all_spots(db)
 
-@app.post("spots", response_model=Spot)
-def create_spot(spot: SpotCreate, db: Session = Depends(get_db)):
-    return create_spot(spot, db)
+@app.post("/spots", response_model=Spot)
+def create_spots(spot: SpotCreate, db: Session = Depends(get_db)):
+    return services.create_spot(db, spot)
