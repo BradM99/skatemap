54925b246bb61aaf3ed262fbdb16957480ad6b8d
bradley <55547314+BradM99@users.noreply.github.com>
2026-02-26 15:40:13 +0000

    removed code from main, mounted spot router to app

--- main.py
+++ main.py
@@ -1,19 +1,11 @@
-from fastapi import FastAPI, Depends
-from sqlalchemy.orm import Session
-import services
-from api_schemas import *
-from db import get_db
+from fastapi import FastAPI
+
+from api import spots
 
 app = FastAPI()
+
+app.include_router(spots.router)
 
 @app.get("/")
 def root():
     return {"message": "API is running"}
-
-@app.get("/spots", response_model=list[Spot])
-def get_all_spots(db: Session = Depends(get_db)):
-    return services.get_all_spots(db)
-
-@app.post("/spots", response_model=Spot)
-def create_spots(spot: SpotCreate, db: Session = Depends(get_db)):
-    return services.create_spot(db, spot)
