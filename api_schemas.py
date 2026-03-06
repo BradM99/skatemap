cc9caa1db6c88e3e9a38be56fe457879d2d2ec27
bradley <55547314+BradM99@users.noreply.github.com>
2026-03-06 21:50:03 +0000

    added update schema to make db operations easier

--- api_schemas.py
+++ api_schemas.py
@@ -1,17 +1,27 @@
 from pydantic import BaseModel
 from uuid import UUID
 
+
 class SpotBase(BaseModel):
     name: str
     description: str
     latitude: float
     longitude: float
 
+
 class SpotCreate(SpotBase):
     pass
 
-class Spot(SpotBase):
+
+class SpotUpdate(BaseModel):
+    name: str | None = None
+    description: str | None = None
+    latitude: float | None = None
+    longitude: float | None = None
+
+
+class SpotRead(SpotBase):
     id: UUID
 
     class Config:
-        from_attribute = True
+        from_attributes = True
