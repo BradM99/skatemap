59c3663ecd36f5e9c341b7ff8dbb867311c2a79a
bradley <55547314+BradM99@users.noreply.github.com>
2026-03-10 22:45:58 +0000

    added fastapi app info

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from config import settings
 
 from api import spots
 
-app = FastAPI()
+app = FastAPI(
+    title=settings.APP_NAME,
+    version=settings.APP_VERSION,
+    description="FastAPI backend for Skatemap",
+    docs_url="/docs",
+    redoc_url="/redoc",
+    openapi_url="/openapi.json"
+)
 
 app.include_router(spots.router)
 
