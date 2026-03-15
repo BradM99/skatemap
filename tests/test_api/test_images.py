6f79cd08d2efcc3a340f2c81c7878041c591616d
bradley <55547314+BradM99@users.noreply.github.com>
2026-03-15 23:39:55 +0000

    pydoc for tests

--- tests/test_api/test_images.py
+++ tests/test_api/test_images.py
@@ -9,15 +9,26 @@ app_client = TestClient(app)
 class TestImagesAPI(TestCase):
 
     def test_add_image(self, client):
-
+        """
+        Test adding an image.
+        """
         pass
 
     def test_delete_image(self, client):
+        """
+        Test deleting an image.
+        """
         pass
 
     def test_list_images(self, client):
+        """
+        Test listing all images.
+        """
         pass
 
     def test_update_image(self, client):
+        """
+        Test updating an image.
+        """
         pass
 
