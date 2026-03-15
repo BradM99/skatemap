e7df102d4737a4f80a36458bc080f1baf21ee3c1
bradley <55547314+BradM99@users.noreply.github.com>
2026-03-15 23:50:28 +0000

    added response codes to endpoints

--- api/spots.py
+++ api/spots.py
@@ -1,3 +1,4 @@
+from http import HTTPStatus
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
@@ -8,7 +9,7 @@ from database import spot_db
 router = APIRouter(prefix="/spots", tags=["spots"])
 
 
-@router.get("/", response_model=list[SpotRead])
+@router.get("/", response_model=list[SpotRead], status_code=HTTPStatus.OK)
 def get_all_spots(db: Session = Depends(get_db)):
     """
     Returns a sequence of spots all spots in the database
@@ -16,7 +17,7 @@ def get_all_spots(db: Session = Depends(get_db)):
     return spot_db.get_all_spots(db)
 
 
-@router.post("/", response_model=SpotRead)
+@router.post("/", response_model=SpotRead, status_code=HTTPStatus.CREATED)
 def create_spot(spot: SpotCreate, db: Session = Depends(get_db)):
     """
     Creates a new spot in the database
@@ -24,7 +25,7 @@ def create_spot(spot: SpotCreate, db: Session = Depends(get_db)):
     return spot_db.create_spot(db, spot)
 
 
-@router.get("/{spot_id}", response_model=SpotRead)
+@router.get("/{spot_id}", response_model=SpotRead, status_code=HTTPStatus.OK)
 def get_spot(spot_id: str, db: Session = Depends(get_db)):
     """
     Gets a spot from the database using the spot_id.
