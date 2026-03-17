c8bdfc3fbf7fdf0f4ec26f7a3c13903291cf88b4
bradley <55547314+BradM99@users.noreply.github.com>
2026-03-17 12:00:32 +0000

    happy path for creating spot image, need to test physical storage of image

--- tests/test_db/test_images_db.py
+++ tests/test_db/test_images_db.py
@@ -15,3 +15,12 @@ class TestImagesDB:
         """
         image = create_spot_image(db, spot.id, "test_image.jpg")
         assert get_spot_images(db, spot.id) == [image]
+
+    def test_upload_image(self, db, spot):
+        """
+        Tests that create_spot_image creates a new image record in the database
+        """
+        image = create_spot_image(db, spot.id, "test_image.jpg")
+        assert image is not None
+        assert image.file_path == "test_image.jpg"
+        assert image.spot_id == spot.id
