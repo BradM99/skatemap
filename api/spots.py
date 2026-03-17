a17113c35941040a2b5ec85938e6a1f7ba2241ff
bradley <55547314+BradM99@users.noreply.github.com>
2026-03-17 11:54:34 +0000

    added upload image and get spot images endpoints

--- api/spots.py
+++ api/spots.py
@@ -1,10 +1,14 @@
+import shutil
 from http import HTTPStatus
-from fastapi import APIRouter, Depends, HTTPException
+from pathlib import Path
+from uuid import UUID
+
+from fastapi import APIRouter, Depends, HTTPException, UploadFile, File
 from sqlalchemy.orm import Session
 
 from database.db import get_db
-from api_schemas import SpotCreate, SpotUpdate, SpotRead
-from database import spot_db
+from api_schemas import SpotCreate, SpotUpdate, SpotRead, ImageRead, ImageCreate
+from database import spot_db, images_db
 from database.db_models import Spot
 
 router = APIRouter(prefix="/spots", tags=["spots"])
@@ -20,12 +24,6 @@ def get_all_spots(db: Session = Depends(get_db)):
 def get_spots_by_bounding_box(db: Session, min_lat: float, max_lat: float, min_lng: float, max_lng: float):
     """
     Gets all spots within a specified boundary. Easily implemented on the FE with map.getBounds() with Leaflet.js
-    :param db:
-    :param min_lat:
-    :param max_lat:
-    :param min_lng:
-    :param max_lng:
-    :return:
     """
     return db.query(Spot).filter(
         Spot.latitude >= min_lat,
@@ -44,7 +42,7 @@ def create_spot(spot: SpotCreate, db: Session = Depends(get_db)):
 
 
 @router.get("/{spot_id}", response_model=SpotRead, status_code=HTTPStatus.OK)
-def get_spot(spot_id: str, db: Session = Depends(get_db)):
+def get_spot(spot_id: UUID, db: Session = Depends(get_db)):
     """
     Gets a spot from the database using the spot_id.
     Raises 404 if spot does not exist.
@@ -53,3 +51,23 @@ def get_spot(spot_id: str, db: Session = Depends(get_db)):
     if spot is None:
         raise HTTPException(status_code=404, detail="Spot not found")
     return spot
+
+@router.get("/spots/{spot_id}/images", response_model=list[ImageRead], status_code=HTTPStatus.OK)
+def get_spot_images(spot_id: UUID, db: Session = Depends(get_db)):
+    return images_db.get_spot_images(db, spot_id)
+
+@router.post("/spots/{spot_id}/images", response_model=ImageRead, status_code=201)
+def upload_image(spot_id: UUID, file: UploadFile = File(...), db: Session = Depends(get_db)):
+    """
+    Uploads a new image for a specific spot.
+    :param spot_id: UUID of the spot to which the image will be attached
+    :param file: File to upload
+    """
+    save_dir = Path("static/images") / str(spot_id)
+    save_dir.mkdir(parents=True, exist_ok=True)
+    file_path = save_dir / file.filename
+
+    with file_path.open("wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    return images_db.create_spot_image(db, spot_id, str(file_path))
