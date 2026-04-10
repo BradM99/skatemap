cd25a399b10a2042213db7c4fa396b1c4598d203
bradley <55547314+BradM99@users.noreply.github.com>
2026-04-10 14:53:12 +0100

    move get_or_404 to db utils

--- database/images_db.py
+++ database/images_db.py
@@ -4,25 +4,13 @@ from sqlalchemy.orm import Session
 from starlette import status
 from starlette.exceptions import HTTPException
 
-from database.db_models import Spot, Image
-
-
-def get_or_404(db: Session, model, object_id: UUID):
-    """
-    Get helper that returns a HTTP 404 if the database object can't be found
-    """
-    obj = db.get(model, object_id)
-    if obj is None:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"{model.__name__} not found"
-        )
-    return obj
+from database.models import Spot, Image
+from database.utils import get_or_404
 
 
 def get_spot_images(db: Session, spot_id: UUID):
     """
-    Gets all images associated with a specific spot
+    Gets all images associated with a specific spot.
     """
     spot = get_or_404(db, Spot, spot_id)
     return spot.images
@@ -47,7 +35,7 @@ def delete_spot_image(db: Session, spot_id: UUID, image_id: UUID) -> Image:
     """
     image = get_or_404(db, Image, image_id)
     if image.spot_id != spot_id:
-        raise HTTPException(status_code=404, detail="Image not found for this spot")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Image not found for this spot")
     db.delete(image)
     db.commit()
     return image
