e5868039e12b30f3b764f9959fab9302058166b8
bradley <55547314+BradM99@users.noreply.github.com>
2026-04-10 14:51:42 +0100

    revert auth, to be redeveloped

--- api/auth.py
+++ api/auth.py
@@ -1,28 +1,3 @@
-from datetime import timedelta
-from typing import Annotated
-from fastapi import APIRouter, Depends, HTTPException
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from sqlalchemy.orm import Session
-from starlette import status
-
-from api_schemas import UserCreate, Token, UserPublic
-from database.db import get_db
-from database.users_db import create_user, authenticate_user, create_access_token
+from fastapi import APIRouter
 
 router = APIRouter(prefix="/auth", tags=["auth"])
-
-oauth2_bearer = OAuth2PasswordBearer(tokenUrl="auth/login")
-
-@router.post("/register", response_model=UserPublic, status_code=status.HTTP_201_CREATED)
-async def register(user_data: UserCreate, db: Session = Depends(get_db)):
-    """
-    Register a new user.
-    """
-    user = create_user(db, user_data.username, user_data.password)
-    return user
-
-@router.post("/login", response_model=Token)
-async def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
-    user = authenticate_user(db, form_data.username, form_data.password)
-    access_token = create_access_token(data={"sub": user.username})
-    return Token(access_token=access_token, token_type="bearer")
