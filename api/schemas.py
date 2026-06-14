531dbdc890428caa66a5c2182e2967c45640bf8b
bradley <55547314+BradM99@users.noreply.github.com>
2026-06-15 00:18:28 +0100

    pydantic user class and password validation

--- api/schemas.py
+++ api/schemas.py
@@ -1,6 +1,8 @@
+import string
 from datetime import datetime
+from typing import Annotated
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, field_validator, ConfigDict
 from uuid import UUID
 
 
@@ -30,8 +32,7 @@ class SpotRead(SpotBase):
     id: UUID
     created_at: datetime
 
-    class ConfigDict:
-        from_attributes = True
+    model_config = ConfigDict(from_attributes=True)
 
 
 class ImageBase(BaseModel):
@@ -50,5 +51,45 @@ class ImageRead(ImageBase):
     spot_id: UUID
     uploaded_at: datetime
 
-    class ConfigDict:
-        from_attributes = True
+    model_config = ConfigDict(from_attributes=True)
+
+class UserRegister(BaseModel):
+    """Schema for registering a new user."""
+    username: Annotated[str, Field(min_length=3, max_length=15)]
+    email: Annotated[str, Field(min_length=3, max_length=254)]
+    password: str
+
+    @field_validator("password")
+    @classmethod
+    def validate_password(cls, value):
+        """
+        More than 12 chars
+        Min 1 Uppercase char
+        Min 1 lowercase char
+        Min 1 digit
+        Min 1 symbol
+        """
+        if len(value) < 12:
+            raise ValueError("Password must be at least 12 characters long")
+        if len(value) > 254:
+            raise ValueError("Password must be at most 254 characters long")
+        if not any(c.isupper() for c in value):
+            raise ValueError("Password must contain at least one uppercase character")
+        if not any(c.islower() for c in value):
+            raise ValueError("Password must contain at least one lowercase character")
+        if not any(c.isdigit() for c in value):
+            raise ValueError("Password must contain at least one digit")
+        if not any(c in string.punctuation for c in value):
+            raise ValueError("Password must contain at least one symbol")
+        return value
+
+class UserRead(BaseModel):
+    """Schema for returning a user from the API, includes database generated fields."""
+    id: UUID
+    username: str
+    email: str
+    created_at: datetime
+
+    model_config = ConfigDict(from_attributes=True)
+
+
