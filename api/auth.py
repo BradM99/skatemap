490748cc7873e8b8450d5bf4b714a9b69ec4f6b3
bradley <55547314+BradM99@users.noreply.github.com>
2026-06-28 19:25:50 +0100

    fixed verify password signature, added get current user

--- api/auth.py
+++ api/auth.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import Session
 
 from api.schemas.users import UserRead, UserRegister, UserLogin
 from core import security
-from core.security import verify_password, create_access_token
+from core.security import verify_password, create_access_token, decode_access_token
 from database import users_db
 from database.db import get_db
 
@@ -28,9 +28,21 @@ def register(user: UserRegister, db: Session = Depends(get_db)):
 @router.post("/login", status_code=HTTPStatus.OK)
 def login(user: UserLogin, db: Session = Depends(get_db)):
     db_user = users_db.get_user_by_email(db, user.email)
-    if not db_user or not verify_password(db_user.hashed_password, user.password):
+    if not db_user or not verify_password(user.password, db_user.hashed_password):
         raise HTTPException(status_code=HTTPStatus.UNAUTHORIZED, detail="Invalid username or password")
 
     token = create_access_token({"sub": str(db_user.id)})
     return {"access_token": token, "token_type": "bearer"}
+
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)) -> UserRead:
+    payload = decode_access_token(token)
+    user_id = payload.get("sub")
+    if not user_id:
+        raise HTTPException(status_code=HTTPStatus.UNAUTHORIZED, detail="Invalid token payload")
+
+    user = users_db.get_user_by_id(db, user_id)
+    if not user:
+        raise HTTPException(status_code=HTTPStatus.UNAUTHORIZED, detail="User not found")
+
+    return user
 
